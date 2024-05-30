0f7c5d2c79894ac2b9e33e6f6ecc061049a94e96
MireyaJO <miregarcia444@gmail.com>
2024-05-29 21:43:55 -0500

    version2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,11 @@
 package org.example;
 public class Main {
     public static void main(String[] args) {
-
-    }
+        //Desde el main creo 5 instancias de la clase (queme los valores)
+        Hoteles instancia1=new Hoteles("La Paz","Chillogallo",100,9.4,true);
+        Hoteles instancia2=new Hoteles("Amanecer","Solanda",150,19.24,true);
+        Hoteles instancia3=new Hoteles("Palmeras","La Tola",200,20.50,false);
+        Hoteles instancia4=new Hoteles("Vista al Mar","El Conde",175,30.10,false);
+        Hoteles instancia5=new Hoteles("Cielo Azul","Quitumbe",10,8.75,true);
+    };
 }

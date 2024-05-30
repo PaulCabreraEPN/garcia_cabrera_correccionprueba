6b617d2129776cdf8348b849103a565361f36384
MireyaJO <miregarcia444@gmail.com>
2024-05-29 22:38:44 -0500

    version3-Creacion de objetos con valores nulos y su seteo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,14 @@ public class Main {
         Hoteles instancia3=new Hoteles("Palmeras","La Tola",200,20.50,false);
         Hoteles instancia4=new Hoteles("Vista al Mar","El Conde",175,30.10,false);
         Hoteles instancia5=new Hoteles("Cielo Azul","Quitumbe",10,8.75,true);
-
+        //Desde el main cree dos objetos con valores nulos y posteriormente setee sus valores.
+        Hoteles Hotel1=new Hoteles();
+        Hotel1.setNombre("Casa Blanca");
+        Hotel1.setDireccion("La Carolina");
+        Hotel1.setNumero_habitaciones(100);
+        Hotel1.setPrecio_noche(16.7);
+        Hotel1.setPiscina(true);
+        Hotel1.imprimir_datos_hotel();
 
     };
 }
